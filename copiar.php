e3b2bc9616d57d3a9c27fdb21a4e3a2b55a9ae3b
robincoello <robincoello@hotmail.com>
2018-11-25 13:57:00 +0100

    add files

---
+++ copiar.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Lista de paginas a extraer la informacion 
+ */
+$paginas = array(
+    "http://www.factux.be",
+    "http://documentacion.lenguaje-latino.org/?c=detalles&f=arco_tangente"
+);
+
+
+
+
+for ($i = 0; $i < count($paginas); $i++) {
+    $web = $paginas[$i];
+    $destino = "pag_$i.html";
+    copiar($web, $destino);
+}
+
+/**
+ * Funcion que extraer la informacion para copiarla en local
+ * @param type $web
+ * @param type $destino
+ */
+function copiar($web, $destino) {
+//abrimos un fichero donde guardar la descarga de la web
+    $fp = fopen("$destino", "w");
+// Se crea un manejador CURL
+    $ch = curl_init();
+// Se establece la URL y algunas opciones
+    curl_setopt($ch, CURLOPT_URL, "$web");
+//determina si descargamos las cabeceras del servidor [0-No mostramos|1-mostramos]
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+//determina si mostramos el resultado en el nevagador [0-mostramos|1-NO mostramos]
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+//determina donde guardar el fichero
+    curl_setopt($ch, CURLOPT_FILE, $fp);
+// Se obtiene la URL indicada
+    curl_exec($ch);
+// Se cierra el recurso CURL y se liberan los recursos del sistema
+    curl_close($ch);
+//se cierra el manejador de ficheros
+    fclose($fp);
+}
+
+?>
